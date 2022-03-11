95488cedbed37d536ae1d153320d7fde7ed75ea5
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-03-11 15:00:33 +0700

    test part 8

--- public/index.js
+++ public/index.js
@@ -1,3 +1,3 @@
-const field = document.getElementById('field')
-console.log(field)
-field.innerHTML = 'Hallo'
+const hallo = dov.getElementById('field')
+console.log(hallo)
+hallo.innerHTML = 'Hallo'
